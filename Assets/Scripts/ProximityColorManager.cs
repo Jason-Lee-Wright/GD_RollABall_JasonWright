19f59df2156e83db1cc07e11c13a222cb5ba06d3
Jason <jason974448@gmail.com>
2025-12-02 21:35:28 -0400

    Final Commit

--- Assets/Scripts/ProximityColorManager.cs
+++ Assets/Scripts/ProximityColorManager.cs
@@ -12,33 +12,25 @@ public class ProximityColorManager : MonoBehaviour
     {
         if (player != null && effectedMaterial1 != null)
         {
-            Debug.Log("DoingSOMETHING");
             effectedMaterial1.SetVector("_PlayerPosition", player.position);
-            Debug.Log(player.position);
             effectedMaterial1.SetFloat("_RevealRadius", revealRadius);
         }
 
         if (player != null && effectedMaterial2 != null)
         {
-            Debug.Log("DoingSOMETHING");
             effectedMaterial2.SetVector("_PlayerPosition", player.position);
-            Debug.Log(player.position);
             effectedMaterial2.SetFloat("_RevealRadius", revealRadius);
         }
 
         if (player != null && effectedMaterial3 != null)
         {
-            Debug.Log("DoingSOMETHING");
             effectedMaterial3.SetVector("_PlayerPosition", player.position);
-            Debug.Log(player.position);
             effectedMaterial3.SetFloat("_RevealRadius", revealRadius);
         }
 
         if (player != null && effectedMaterial4 != null)
         {
-            Debug.Log("DoingSOMETHING");
             effectedMaterial4.SetVector("_PlayerPosition", player.position);
-            Debug.Log(player.position);
             effectedMaterial4.SetFloat("_RevealRadius", revealRadius);
         }
     }
